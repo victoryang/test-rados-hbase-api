775062fb9b981feb191fd598270f22fe3eb0c9be
victoryang <yangtao3601@126.com>
2018-01-02 03:44:23 -0500

    test

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 "fmt"
+"github.com/tsuna/gohbase"
 )
 
 func main(){
+client := gohbase.NewClient("localhost")
+values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{aaa}}}
+putRequest, err := hrpc.NewPutStr(context.Background(), "test", "rowkey1", values)
+if err != nil {
+}
 fmt.Println("hello world")
 }

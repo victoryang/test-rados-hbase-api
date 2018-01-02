ee9a4532eb9a13654211ff642da8b4a98d50bf54
victoryang <yangtao3601@126.com>
2018-01-02 03:53:24 -0500

    test

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main(){
 client := gohbase.NewClient("localhost")
-values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{"aaa"}}}
+values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{0}}}
 putRequest, err := hrpc.NewPutStr(context.Background(), "test", "rowkey1", values)
 if err != nil {
 fmt.Println(err)

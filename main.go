f6465ae8515eeb0e9380c1bab2bb56e2ca1575e2
victoryang <yangtao3601@126.com>
2018-01-02 03:50:20 -0500

    test

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 "fmt"
+"context"
 "github.com/tsuna/gohbase"
+"github.com/cannium/gohbase/hrpc"
 )
 
 func main(){
 client := gohbase.NewClient("localhost")
-values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{aaa}}}
+values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{"aaa"}}}
 putRequest, err := hrpc.NewPutStr(context.Background(), "test", "rowkey1", values)
 if err != nil {
+fmt.Println(err)
 }
 fmt.Println("hello world")
 }

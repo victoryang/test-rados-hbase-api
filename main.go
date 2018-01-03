63ffe5c69a92a5ee946f0882cc26d6ef6df7a488
victoryang <yangtao3601@126.com>
2018-01-03 13:03:54 +0800

    fix

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tsuna/gohbase/hrpc"
 )
 
+// Insert a cell
 func testHbaseCase1 () {
 	client := gohbase.NewClient("localhost")
 	values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{0}}}
@@ -23,6 +24,7 @@ func testHbaseCase1 () {
 	client.Close()
 }
 
+// Get an entire row
 func testHbaseCase2() {
 	client := gohbase.NewClient("localhost")
 	getRequest, err := hrpc.NewGetStr(context.Background(), "test", "rowkey1")
@@ -38,11 +40,33 @@ func testHbaseCase2() {
 		fmt.Println(string(cell.Qualifier))
 		fmt.Println(string(cell.Value))
 	}
+	client.Close()
+}
+
+// Get a specific cell
+func testHbaseCase3() {
+	client := gohbase.NewClient("localhost")
+	family := map[string][]string{"cf1": []string{"col1"}}
+	getRequest, err := hrpc.NewGetStr(context.Background(), "test", "rowkey1", hrpc.Families(family))
+	if err != nil {
+	}
+	getRsp, err := client.Get(getRequest)
+	if err != nil {
+	}
+	for _, cell := range getRsp.Cells {
+		fmt.Println(string(cell.Row))
+		fmt.Println(string(cell.Family))
+		fmt.Println(string(cell.Qualifier))
+		fmt.Println(string(cell.Value))
+	}
+	client.Close()
 }
 
 func main(){
 	testHbaseCase1()
 	time.Sleep(1)
 	testHbaseCase2()
+	time.Sleep(1)
+	testHbaseCase3()
 	fmt.Println("test finished, leaving...")
 }

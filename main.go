6563a41f55e4520e15690eee80bbf47472597e5a
victoryang <yangtao3601@126.com>
2018-01-03 11:24:50 +0800

    fix

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ func testHbaseCase1 () {
 	values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{0}}}
 	putRequest, err := hrpc.NewPutStr(context.Background(), "test", "rowkey1", values)
 	if err != nil {
-		fmt.Println(err)
+		//fmt.Println(err)
 	}
 	rsp, err := client.Put(putRequest)
 	if err != nil {
 		fmt.Println(rsp)
-		fmt.Println(err)
+		//fmt.Println(err)
 	}
 	client.Close()
 }

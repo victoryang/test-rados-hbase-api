ac4bc4aa2e879a22d94d59eb497775150ad08e94
victoryang <yangtao3601@126.com>
2018-01-03 11:20:41 +0800

    add hbase test2

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 "fmt"
 "context"
+"time"
 "github.com/tsuna/gohbase"
 "github.com/tsuna/gohbase/hrpc"
 )
 
-func main(){
+func testHbaseCase1 () {
 	client := gohbase.NewClient("localhost")
 	values := map[string]map[string][]byte{"cf1": map[string][]byte{"col1": []byte{0}}}
 	putRequest, err := hrpc.NewPutStr(context.Background(), "test", "rowkey1", values)
@@ -20,5 +21,22 @@ func main(){
 		fmt.Println(err)
 	}
 	client.Close()
-	fmt.Println("hello world")
+}
+
+func testHbaseCase2() {
+	client := gohbase.NewClient("localhost")
+	getRequest, err := hrpc.NewGetStr(context.Background(), "test", "rowkey1")
+	if err != nil {
+	}
+	getRsp, err := client.Get(getRequest)
+	if err !=nil {
+	}
+	fmt.Println(getrsp)
+}
+
+func main(){
+	testHbaseCase1()
+	time.Sleep(1)
+	testHbaseCase2()
+	fmt.Println("test finished, leaving...")
 }
